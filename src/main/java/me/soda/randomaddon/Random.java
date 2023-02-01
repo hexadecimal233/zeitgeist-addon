b38fbdc4d75d2e777bda3203c0ad53b5ba2a4ddb
ThebestkillerTBK <2593828650@qq.com>
2023-02-02 01:01:53 +0800

    jetpack

--- src/main/java/me/soda/randomaddon/Random.java
+++ src/main/java/me/soda/randomaddon/Random.java
@@ -24,6 +24,7 @@ public class Random extends MeteorAddon {
 
         // My
         modules.add(new HackerDetector());
+        modules.add(new Jetpack());
 
         // Allah
         modules.add(new BackTP());
